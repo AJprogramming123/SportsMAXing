6fb3b083f89423726c2308e6b683990e3f005b9a
AJprogramming123 <jaimesandres2023@gmail.com>
2024-07-01 23:28:47 -0400

    created platform for title

--- JavaScript/loginscreen.js
+++ JavaScript/loginscreen.js
@@ -1,21 +1,26 @@
 
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-
     event.preventDefault();
 
-  
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
+    
     const validUsername = "John_Doe";
     const validPassword = "abc123";
 
-    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
     if (username === validUsername && password === validPassword) {
+        
+        const fullName = "John Doe"; 
+        localStorage.setItem('fullName', fullName);
 
-        window.location.href = 'HTML_LOGGED/home.html';
-
+        
+        
+        window.location.href = 'index.html';
     } else {
-        alert('Invalid username or password.\n\nTRY:\nUsername: John_Doe\nPassword: abc123');
+        alert('Invalid username or password. Please try again.');
     }
 });
+
+
+
