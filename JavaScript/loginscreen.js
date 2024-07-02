b6af07c13eda41871e0fd47e2cc08f8391e5f395
AJprogramming123 <jaimesandres2023@gmail.com>
2024-07-02 17:06:36 -0400

    Missing button but added promotion page

--- JavaScript/loginscreen.js
+++ JavaScript/loginscreen.js
@@ -11,14 +11,14 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     if (username === validUsername && password === validPassword) {
         
-        const fullName = "John Doe"; 
+        const fullName = "Welcome Back! John Doe"; 
         localStorage.setItem('fullName', fullName);
 
         
         
         window.location.href = 'index.html';
     } else {
-        alert('Invalid username or password. Please try again.');
+        alert('Invalid username or password. Please try again.\n\nTry:\n\nUsername: John_Doe\n\nPassword: abc123\n');
     }
 });
 
