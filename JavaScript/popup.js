f581880739657c94d0cd16f267ae4778dedef61d
AJprogramming123 <jaimesandres2023@gmail.com>
2024-07-02 13:54:42 -0400

    Completed title and popup (Last potions of JS)

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -1,15 +1,4 @@
-document.querySelector('.ANIME').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'block';
-    document.getElementById('popup').style.display = 'block';
-});
-
-document.getElementById('popup-close').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'none';
-    document.getElementById('popup').style.display = 'none';
-});
-
-document.getElementById('overlay').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'none';
-    document.getElementById('popup').style.display = 'none';
-});
-
+function togglePopup()
+{
+    document.getElementById("popup-1").classList.toggle("active");
+}
